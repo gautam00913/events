a8455af5cd322c250ae3ee0932ebaf5981f9ae2f
Gautier DJOSSOU <gautierdjossou@gmail.com>
2023-03-24 10:29:51 +0100

    correction de lien sur le mail

--- resources/views/emails/new-event-added.blade.php
+++ resources/views/emails/new-event-added.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Bonjour l'administration de {{ config('app.name') }}
 
-L'évènement [**{{ $event->title }}**](route('events.index', ["event" => $event->id])) vient d'être ajouté sur le site
+L'évènement [**{{ $event->title }}**]({{ route('events.index', ["event" => $event->id]) }}) vient d'être ajouté sur le site
 par **{{ $organizer->name }}**.
 
 @component('mail::button', ['url' => route('events.index', ["event" => $event->id]), 'color' => 'purple'])
